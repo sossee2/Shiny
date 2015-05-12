854f11bc2d35a1fdef1e574820226d9a5d2fdae4
Sam <sam.g.bachour@gmail.com>
2015-05-12 21:42:59 +1000

    Test

--- Dops/App/EmployeeForm/efController.js
+++ Dops/App/EmployeeForm/efController.js
@@ -1,4 +1,14 @@
 ﻿angularFormsApp.controller('efController',
     function efController($scope, efService) {
         $scope.employee = efService.employee;
+
+        $scope.departments = [
+            "Engineering",
+            "Marketing",
+            "Finance",
+            "Administration"];
+
+        $scope.submitForm = function () {
+
+        }
     });
